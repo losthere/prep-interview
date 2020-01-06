c50aabd16be816999cc0328c5b12276a5d668570
-, Swati <swati@optum.com>
2020-01-06 19:44:38 +0530

    dk

---
+++ new 2.java
@@ -0,0 +1,21 @@
+Clone binay tree with random pointers
+https://www.techiedelight.com/clone-a-binary-tree-with-random-pointers/
+
+https://www.glowtouch.com/test-driven-development-vs-behavior-driven-development/
+difference btween BDD and TDD-
+in TDD, in acceptance criteria is written in gherkins which is like if else condition. so we can say that automation started from here itself.
+in BDD it is written in plain english.
+
+swapping of linkedlist nodes
+sorting
+
+difference between HashMap,TreeMap,LinkedHashMap
+hashMap-no order-fastest-0(1)-one null key possible
+treemap-sorted(internally implemented using red black tree)-0(logn)-no null key
+LinkedHashMap-insertion order-0(1)-one null key possible
+
+https://artoftesting.com/interviewSection/sql-queries-for-interview.html
+https://javarevisited.blogspot.com/2018/02/top-20-spring-rest-interview-questions-answers-java.html
+https://www.journaldev.com/3633/hibernate-interview-questions-and-answers#entity-final-class
+https://javarevisited.blogspot.com/2017/10/differences-between-requestparam-and-pathvariable-annotations-spring-mvc.html
+
